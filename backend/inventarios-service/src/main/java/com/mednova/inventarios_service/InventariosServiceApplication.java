ca91752bdddccf17fb2ea1427373a29e8b5f16e6
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-05-06 13:54:01 -0400

    CREATE - backend spring projects

---
+++ backend/inventarios-service/src/main/java/com/mednova/inventarios_service/InventariosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mednova.inventarios_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventariosServiceApplication.class, args);
+	}
+
+}
---
+++ backend/inventarios-service/src/test/java/com/mednova/inventarios_service/InventariosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mednova.inventarios_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
