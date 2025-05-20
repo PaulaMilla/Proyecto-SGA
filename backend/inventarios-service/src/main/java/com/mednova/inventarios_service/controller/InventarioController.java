7e013c24efa0d07dc7ae136c3c3f8487a3c610ca
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-05-20 16:59:35 -0400

    prueba #5 workflow

--- backend/inventarios-service/src/main/java/com/mednova/inventarios_service/controller/InventarioController.java
+++ backend/inventarios-service/src/main/java/com/mednova/inventarios_service/controller/InventarioController.java
@@ -13,6 +13,8 @@ import java.util.Optional;
 @RequestMapping("/api/inventarios")
 public class InventarioController {
 
+
+    
     private final InventarioService inventarioService;
 
     public InventarioController(InventarioService inventarioService) {
