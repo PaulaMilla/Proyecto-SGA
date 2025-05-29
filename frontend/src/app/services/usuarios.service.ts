2bbaab55f6525c4e354ccea0214d3c8c93d55d95
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-05-28 21:20:37 -0400

    avances frontend

---
+++ frontend/src/app/services/usuarios.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor(private http: HttpClient) { }
+
+  apiUrl = 'http://127.0.0.1:51495/api/usuarios';
+
+  
+
+}
