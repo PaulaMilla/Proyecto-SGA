c1c5740e0915640bcbf34663562bfac1fee0f15e
Katalina-Star <katalina.ramirez2101@alumnos.ubiobio.cl>
2025-06-02 00:06:36 -0400

    paciente

---
+++ backend/pacientes-service/src/test/java/com/mednova/pacientes_service/PacientesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mednova.pacientes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacientesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
