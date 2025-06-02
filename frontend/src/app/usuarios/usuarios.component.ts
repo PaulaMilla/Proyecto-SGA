9bf0e59700b03a9924b4288c62f8a8e10b547659
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-06-02 08:37:00 -0400

    prueba ventas

--- frontend/src/app/usuarios/usuarios.component.ts
+++ frontend/src/app/usuarios/usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario, UsuariosService } from '../services/usuarios.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './usuarios.component.scss'
 })
 export class UsuariosComponent {
+  usuarios: Usuario[] = [];
+  
+    constructor(private usuarioService: UsuariosService) {}
+  
+    ngOnInit() {
+      this.usuarioService.obtenerTodas().subscribe(data => {
+        this.usuarios = data;
+      });
+    }
+
 
 }
