194e39cd9d0a0a32709006ed48ebc041936e8261
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-06-17 22:20:37 -0400

    fix error back

--- backend/inventarios-service/src/main/java/com/mednova/inventarios_service/config/FileUploadConfig.java
+++ backend/inventarios-service/src/main/java/com/mednova/inventarios_service/config/FileUploadConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.util.unit.DataSize;
 
-import javax.servlet.MultipartConfigElement;
-
 @Configuration
 public class FileUploadConfig {
 
