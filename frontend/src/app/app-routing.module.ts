6e28bd3439d88504ff8fb66529039d3c27cca05d
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-06-17 20:04:32 -0400

    prueba ruta, subida de archivo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VentasComponent } from './ventas/ventas.component';
+import { UploadComponent } from './inventario/upload/upload.component';
 
 const routes: Routes = [
   {path: 'ventas', component: VentasComponent},
-  { path: 'usuarios', loadChildren: () => import('./usuarios/usuarios.module').then(m => m.UsuariosModule) }
+  { path: 'usuarios', loadChildren: () => import('./usuarios/usuarios.module').then(m => m.UsuariosModule)},
+  {path: 'upload', component: UploadComponent}
 ];
 
 @NgModule({
