39bd805def5fbc567d1c0549f3e3acc1c7ab7abb
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-06-21 20:23:29 -0400

    cambios visuales login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
-import { FormBuilder,FormGroup, Validators } from '@angular/forms';
-import { jwtDecode } from 'jwt-decode';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
+  showModal = false;
+  modalMessage = '';
+  isSuccess = false;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router){}
-  
-  ngOnInit(): void{
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
-    })
+    });
   }
 
   onSubmit(): void {
@@ -28,15 +34,37 @@ export class LoginComponent {
         next: (res) => {
           localStorage.setItem('token', res.token);
           localStorage.setItem('email', res.email);
-          localStorage.setItem('rol', res.rol); 
-          this.router.navigate(['/home']); 
+          localStorage.setItem('rol', res.rol);
+
+          this.modalMessage = '¡Bienvenido al sistema!';
+          this.isSuccess = true;
+          this.showModal = true;
         },
         error: (err) => {
-          alert('Credenciales inválidas');
-          console.log(err);
+          this.modalMessage = 'Credenciales inválidas. Intenta nuevamente.';
+          this.isSuccess = false;
+          this.showModal = true;
+          console.error(err);
         }
       });
     }
   }
 
+  closeModal(): void {
+    const modal = document.querySelector('.custom-modal');
+    if (modal) {
+      modal.classList.add('hide');
+      setTimeout(() => {
+        this.showModal = false;
+        if (this.isSuccess) {
+          this.router.navigate(['/home']);
+        }
+      }, 300);
+    } else {
+      this.showModal = false;
+      if (this.isSuccess) {
+        this.router.navigate(['/home']);
+      }
+    }
+  }
 }
