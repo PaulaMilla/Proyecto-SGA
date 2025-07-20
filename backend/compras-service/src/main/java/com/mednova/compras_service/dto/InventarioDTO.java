9102aa7933a4fafec4e6be12731798238015c6e7
PaulaMilla <paula.veloso2201@alumnos.ubiobio.cl>
2025-07-20 17:37:27 -0400

    avances back compra

---
+++ backend/compras-service/src/main/java/com/mednova/compras_service/dto/InventarioDTO.java
@@ -0,0 +1,61 @@
+package com.mednova.compras_service.dto;
+
+public class InventarioDTO {
+    private Long productoId;
+    private Integer cantidadDisponible;
+    private String lote;
+    private String fechaVencimiento;
+    private String ubicacion;
+    private Integer farmaciaId;
+
+    public InventarioDTO() {}
+
+
+    public Long getProductoId() {
+        return productoId;
+    }
+
+    public void setProductoId(Long productoId) {
+        this.productoId = productoId;
+    }
+
+    public Integer getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+
+    public void setCantidadDisponible(Integer cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public Integer getFarmaciaId() {
+        return farmaciaId;
+    }
+
+    public void setFarmaciaId(Integer farmaciaId) {
+        this.farmaciaId = farmaciaId;
+    }
+}
--- backend/compras-service/src/main/java/com/mednova/compras_service/service/CompraService.java
+++ backend/compras-service/src/main/java/com/mednova/compras_service/service/CompraService.java
@@ -1,5 +1,6 @@
 package com.mednova.compras_service.service;
 
+import com.mednova.compras_service.dto.InventarioDTO;
 import com.mednova.compras_service.model.Compra;
 import com.mednova.compras_service.model.DetalleCompra;
 import com.mednova.compras_service.model.EstadoCompra;
@@ -7,6 +8,7 @@ import com.mednova.compras_service.repository.CompraRepository;
 import com.mednova.compras_service.repository.ProveedorRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -16,13 +18,14 @@ import java.util.Optional;
 public class CompraService {
     private final CompraRepository compraRepository;
     private final ProveedorRepository proveedorRepository;
-
-    public CompraService(CompraRepository compraRepository, ProveedorRepository proveedorRepository) {
+    private final WebClient webClient;
+    public CompraService(WebClient.Builder builder, CompraRepository compraRepository, ProveedorRepository proveedorRepository) {
+        this.webClient = builder.baseUrl("http://inventarios-service.inventarios.svc.cluster.local").build();
         this.compraRepository = compraRepository;
         this.proveedorRepository = proveedorRepository;
     }
 
-    public Compra crearCompra(Compra compra) {
+    public Compra crearCompra(Compra compra, String bodega, int idFarmacia) {
         // Calcular total y subtotales
         BigDecimal total = BigDecimal.ZERO;
 
@@ -31,6 +34,16 @@ public class CompraService {
             detalle.setSubtotal(subtotal);
             detalle.setCompra(compra); // Setear relación inversa
             total = total.add(subtotal);
+
+            InventarioDTO dto = new InventarioDTO();
+            dto.setProductoId(detalle.getProductoId());
+            dto.setCantidadDisponible(detalle.getCantidad());
+            dto.setLote(detalle.getLote());
+            dto.setFechaVencimiento(detalle.getFechaVencimiento().toString());
+            dto.setUbicacion(bodega);
+            dto.setFarmaciaId(idFarmacia);
+
+            agregarStockDesdeCompra(dto);
         }
 
         compra.setTotal(total);
@@ -56,5 +69,18 @@ public class CompraService {
 
         compra.setEstado(EstadoCompra.ANULADA);
         return compraRepository.save(compra);
+    }
+
+    public void agregarStockDesdeCompra(InventarioDTO inventario){
+        try{
+            webClient.post()
+                    .uri("/api/inventarios")
+                    .bodyValue(inventario)
+                    .retrieve()
+                    .bodyToMono(Void.class)
+                    .block();
+        }catch (Exception e){
+            throw new RuntimeException("Error al agregar stock de compra");
+        }
     }
 }
