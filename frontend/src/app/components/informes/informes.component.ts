570c957ff666f4f8eaffea422ae392de8f31f4bf
patito31 <antonia.yevenes1901@alumnos.ubiobio.cl>
2025-07-20 21:11:56 -0400

    informes

--- frontend/src/app/components/informes/informes.component.ts
+++ frontend/src/app/components/informes/informes.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {VentaConNombresYDetalles} from "../../ventas/model/venta-con-detalles-view.dto";
+import FileSaver from "file-saver";
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-informes',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class InformesComponent {
 
+  ventas: VentaConNombresYDetalles[] = [];
+
+  exportarVentasExcel(): void {
+    const dataParaExcel: any[] = [];
+
+    this.ventas.forEach(v => {
+      v.detalles.forEach(d => {
+        dataParaExcel.push({
+          'ID Venta': v.id,
+          'Fecha Venta': v.fechaVenta,
+          'Paciente': v.nombrePaciente,
+          'Vendedor': `${v.nombreUsuario} ${v.apellidoUsuario}`,
+          'Producto': d.nombreProducto,
+          'Cantidad': d.cantidad,
+          'Precio Unitario': d.precioUnitario,
+          'Subtotal': d.subtotal,
+          'Total Venta': v.total
+        });
+      });
+    });
+
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(dataParaExcel);
+    const workbook: XLSX.WorkBook = { Sheets: { 'Ventas': worksheet }, SheetNames: ['Ventas'] };
+
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob: Blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    FileSaver.saveAs(blob, 'ventas_historicas.xlsx');
+  }
 }
+
