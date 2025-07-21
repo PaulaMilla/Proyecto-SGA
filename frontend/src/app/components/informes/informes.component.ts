25822385cbc7dd25195ac8e6d0029708c3dcf012
patito31 <antonia.yevenes1901@alumnos.ubiobio.cl>
2025-07-20 21:30:55 -0400

    informes

--- frontend/src/app/components/informes/informes.component.ts
+++ frontend/src/app/components/informes/informes.component.ts
@@ -1,16 +1,75 @@
-import { Component } from '@angular/core';
-import {VentaConNombresYDetalles} from "../../ventas/model/venta-con-detalles-view.dto";
+import {Component, OnInit} from '@angular/core';
+import {DetalleVentaConNombre, VentaConNombresYDetalles} from "../../ventas/model/venta-con-detalles-view.dto";
 import FileSaver from "file-saver";
 import * as XLSX from 'xlsx';
+import {VentasService} from "../../ventas/services/ventas.service";
 
 @Component({
   selector: 'app-informes',
   templateUrl: './informes.component.html',
   styleUrl: './informes.component.scss'
 })
-export class InformesComponent {
+export class InformesComponent implements OnInit{
 
   ventas: VentaConNombresYDetalles[] = [];
+
+  constructor(private ventasService: VentasService) {}
+
+  ngOnInit(): void {
+    this.cargarVentas(); // También podrías mover esto al botón si prefieres
+  }
+
+  cargarVentas(): void {
+    this.ventasService.obtenerTodas().subscribe((ventas: any[]) => {
+      const ventasConTodo: VentaConNombresYDetalles[] = [];
+
+      ventas.forEach((v) => {
+        const ventaExtendida: VentaConNombresYDetalles = {
+          id: v.id,
+          pacienteId: v.pacienteId,
+          idUsuarioVendedor: v.idUsuarioVendedor,
+          fechaVenta: v.fechaVenta,
+          total: v.total,
+          nombrePaciente: '',
+          nombreUsuario: '',
+          apellidoUsuario: '',
+          detalles: []
+        };
+        // Obtener nombres por ID
+        this.ventasService.getPacientePorId(v.pacienteId).subscribe((p) => {
+          ventaExtendida.nombrePaciente = p.nombre;
+        });
+
+        this.ventasService.getUsuarioPorId(v.idUsuarioVendedor).subscribe((u) => {
+          ventaExtendida.nombreUsuario = u.nombre;
+          ventaExtendida.apellidoUsuario = u.apellido;
+        });
+
+        // Obtener detalles de la venta
+        this.ventasService.getDetallesPorVentaId(v.id).subscribe((detalles: any[]) => {
+          detalles.forEach((d) => {
+            const detalleExtendido: DetalleVentaConNombre = {
+              productoId: d.productoId,
+              cantidad: d.cantidad,
+              precioUnitario: d.precioUnitario,
+              subtotal: d.subtotal,
+              nombreProducto: ''
+            };
+
+            this.ventasService.getProductoPorId(d.productoId).subscribe((prod) => {
+              detalleExtendido.nombreProducto = prod.nombre;
+            });
+
+            ventaExtendida.detalles.push(detalleExtendido);
+          });
+        });
+
+        ventasConTodo.push(ventaExtendida);
+      });
+
+      this.ventas = ventasConTodo;
+    });
+  }
 
   exportarVentasExcel(): void {
     const dataParaExcel: any[] = [];
